c81cf525cbcc359e0b6912480ba132bbf4543834
ral-itong <bawbawbiscuit@gmail.com>
2016-04-13 17:06:07 +0800

    Added skeleton for whole game

--- GameOfTheGenerals/GameRelated/Objects/Game.cs
+++ GameOfTheGenerals/GameRelated/Objects/Game.cs
@@ -1,24 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 
 namespace GameOfTheGenerals
 {
     public class Game
     {
-        IManualResetEvent _resetEvent;
-        ISocket _otherPlayerSocket;
+        private int _portNumber;
+        private ISocket _hostSocket;
+        private ISocket _clientSocket;
 
 
-        public void ListenToClients()
+
+        public Game()
         {
-            ClientSocketListener socketListener = new ClientSocketListener();
-            socketListener.OtherPlayerConnected = _resetEvent;
-            socketListener.StartListening();
-            _resetEvent.Set();
-            _otherPlayerSocket = socketListener.OtherPlayerSocketAdapter;
+            _portNumber = 10801;
+            HostSocket = new SocketAdapter(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         }
+
+        public void StartGame()
+        {
+            ListenForClients();
+            SetupBoard();
+        }
+
+        private void ListenForClients()
+        {
+            TcpUtil.ListenForClients(HostSocket, _portNumber, new AsyncCallback(AcceptCallBack));
+        }
+
+
+        private void SetupBoard()
+        {
+
+        }
+
+        private void AcceptCallBack(IAsyncResult ar)
+        {
+            ExtractOtherPlayerSocket(ar);
+            WaitForClientsReadyMessage();
+        }
+
+        private void WaitForClientsReadyMessage()
+        {
+            TcpUtil.Listen(new AsyncCallback(PopulateBoardWithClientPieces), _clientSocket);
+        }
+
+        public void PopulateBoardWithClientPieces(IAsyncResult ar)
+        {
+            StateObject state = (StateObject)ar.AsyncState;
+        }
+
+        private void ExtractOtherPlayerSocket(IAsyncResult ar)
+        {
+            ISocket listener = ar.AsyncState as ISocket;
+            _clientSocket = listener.EndAccept(ar);
+        }
+
+        public ISocket HostSocket
+        {
+            get
+            {
+                return _hostSocket;
+            }
+
+            set
+            {
+                _hostSocket = value;
+            }
+        }
+
+
+
 
 
     }
--- GameOfTheGeneralsUnitTests/GameTests.cs
+++ GameOfTheGeneralsUnitTests/GameTests.cs
@@ -1,5 +1,6 @@
 ﻿using GameOfTheGenerals;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,7 +15,23 @@ namespace GameOfTheGeneralsUnitTests
         public void GameShouldBeAbleToListenToClients()
         {
             Game game = new Game();
-            game.ListenToClients();
+
+            var mockSocket = new Mock<ISocket>();
+            var mockAsyncResult = new Mock<IAsyncResult>();
+            var mockTcpUtil = new Mock<TcpUtil>();
+            
+
+            mockAsyncResult.Setup(foo => foo.AsyncState).Returns(mockSocket.Object);
+            mockSocket.Setup(foo => foo
+            .BeginAccept(
+                It.IsAny<AsyncCallback>(),
+                It.IsAny<ISocket>())
+                )
+            .Callback<AsyncCallback, ISocket >(
+                (asyncCallback, socket) => asyncCallback.Invoke(mockAsyncResult.Object));
+
+            game.HostSocket = mockSocket.Object;
+            game.StartGame();
         }
 
 
